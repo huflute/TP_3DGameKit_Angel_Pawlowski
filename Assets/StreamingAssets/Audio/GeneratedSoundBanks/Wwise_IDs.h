4bf8b2388ba69e82e8dbef30f00a157eac46d6da
huflute <hugoangelgon@gmail.com>
2023-01-12 21:33:07 +0100

    Import et Events Hugo

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,13 +14,47 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMB_LOOP_WIND_GUST_01 = 2425561832U;
+        static const AkUniqueID PLAY_AMB_SPACESHIP = 3213331111U;
+        static const AkUniqueID PLAY_CHOMPER_ATTACK = 931488463U;
+        static const AkUniqueID PLAY_CHOMPER_VOX_BEFORE_ATTACK = 3519138347U;
         static const AkUniqueID PLAY_HEALTHCRATE_IDLE = 1840834974U;
         static const AkUniqueID PLAY_HEALTHCRATE_OPEN = 3862797904U;
+        static const AkUniqueID PLAY_MC_FOL_ATTACK = 4094144955U;
+        static const AkUniqueID PLAY_MC_FOL_JUMP = 3212184235U;
+        static const AkUniqueID PLAY_MC_FOL_LAND = 2037670792U;
+        static const AkUniqueID PLAY_MC_FOL_ROLL = 1540365902U;
+        static const AkUniqueID PLAY_MC_FOL_RUN = 733683298U;
         static const AkUniqueID PLAY_MC_JUMP = 3022734957U;
         static const AkUniqueID PLAY_MC_LAND = 1593937298U;
         static const AkUniqueID PLAY_MC_RUN = 1600895284U;
+        static const AkUniqueID PLAY_MC_SHIELD = 1600902502U;
+        static const AkUniqueID PLAY_MC_SHIELD_OFF = 951255116U;
         static const AkUniqueID PLAY_MC_VOX_DEATH = 634444711U;
         static const AkUniqueID PLAY_MC_VOX_PAIN = 3917663999U;
+        static const AkUniqueID PLAY_MONSTER_VANISH = 803911630U;
+        static const AkUniqueID PLAY_RFX_COMPUTER = 3056483954U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_BODYFALL_MUD = 1499078611U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_BODYFALL_STONE = 2386186782U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_HIT = 1602398710U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_RUN_MUD = 1809433285U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_RUN_STONE = 3729170868U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_SPIN = 281344347U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_VOX_DEATH = 237106917U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_VOX_GRUNT = 46056053U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_VOX_HIT = 201430788U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_VOX_SPOTS = 41618512U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_WALK_MUD = 2960261907U;
+        static const AkUniqueID PLAY_SMALL_MONSTER_WALK_STONE = 1451592030U;
+        static const AkUniqueID PLAY_SPITTER_ATTACK = 3913336272U;
+        static const AkUniqueID PLAY_SWITCH_CRYSTALS_ACTIVATION = 3451021523U;
+        static const AkUniqueID PLAY_SWITCH_CRYSTALS_IDLE_AFTERACTIVATION = 3013028862U;
+        static const AkUniqueID PLAY_SWITCH_CRYSTALS_IDLE_BEFOREACTIVATION = 1933987681U;
+        static const AkUniqueID PLAY_SWITCH_STONE_ACTIVATION = 2556341799U;
+        static const AkUniqueID PLAY_SWITCH_STONE_IDLE = 717478169U;
+        static const AkUniqueID PLAY_TELEPORTER_IDLE = 476276007U;
+        static const AkUniqueID PLAY_UI_NAVIGATE = 2193989514U;
+        static const AkUniqueID PLAY_UI_PRESS_OTHER = 3014998451U;
+        static const AkUniqueID PLAY_UI_PRESS_PLAY = 2377842133U;
         static const AkUniqueID PLAY_WEAPON_IDLE = 723528203U;
         static const AkUniqueID PLAY_WEAPON_IMPACT = 3739832523U;
         static const AkUniqueID PLAY_WEAPON_PICKUP = 1954136465U;
@@ -28,6 +62,12 @@ namespace AK
         static const AkUniqueID PLAY_WEAPON_WHOOSH_COMBO02 = 905597478U;
         static const AkUniqueID PLAY_WEAPON_WHOOSH_COMBO03 = 905597479U;
         static const AkUniqueID PLAY_WEAPON_WHOOSH_COMBO04 = 905597472U;
+        static const AkUniqueID STOP_AMB_SPACESHIP = 3876046333U;
+        static const AkUniqueID STOP_MC_SHIELD = 616065004U;
+        static const AkUniqueID STOP_SWITCH_CRYSTALS_IDLE_AFTERACTIVATION = 4212421580U;
+        static const AkUniqueID STOP_SWITCH_CRYSTALS_IDLE_BEFOREACTIVATION = 3192261611U;
+        static const AkUniqueID STOP_SWITCH_STONE_IDLE = 3416772595U;
+        static const AkUniqueID STOP_TELEPORTER_IDLE = 2863201769U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -61,7 +101,15 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID INTERACTABLES = 181270742U;
         static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MC = 1685527061U;
+        static const AkUniqueID SMALL_MONSTERS = 4015708584U;
+        static const AkUniqueID SWITCH = 3202316517U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WEAPON = 3893417221U;
     } // namespace BANKS
 
     namespace BUSSES
