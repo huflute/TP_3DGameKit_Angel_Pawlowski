c0b298ce94798c725f821fad561bb2be885ca8d7
huflute <hugoangelgon@gmail.com>
2023-01-13 20:09:07 +0100

    Fin de journée

--- Assets/Crystal_Switch_Audio_Player.cs
+++ Assets/Crystal_Switch_Audio_Player.cs
@@ -6,20 +6,11 @@ namespace Gamekit3D
 {
     public class Crystal_Switch_Audio_Player : MonoBehaviour
     {
-        public void PlayIdleBeforeActivation()
-        {
-            AkSoundEngine.PostEvent("Play_Switch_Crystals_Idle_BeforeActivation", this.gameObject);
-            Debug.Log("Play_Switch_Crystals_Idle_BeforeActivation");
-        }
-        public void PlayIdleAfterActivation()
-        {
-            AkSoundEngine.PostEvent("Play_Switch_Crystals_Idle_AfterActivation", this.gameObject);
-            Debug.Log("Play_Switch_Crystals_Idle_AfterActivation");
-        }
+        
         public void Activation()
         {
             AkSoundEngine.PostEvent("Play_Switch_Crystals_Activation", this.gameObject);
-            Debug.Log("Play_Switch_Crystals_Activation");
+            
         }
         // Start is called before the first frame update
         void Start()
